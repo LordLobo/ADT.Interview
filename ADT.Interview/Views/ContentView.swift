b90d373b6789f295adb6721cfb3838f2ccef4b8d
Dan Giralte <dangiralte@blinkupapp.com>
2020-09-15 15:28:57 -0500

    more styling, slight code reorg

--- ADT.Interview/Views/ContentView.swift
+++ ADT.Interview/Views/ContentView.swift
@@ -11,40 +11,48 @@ import SwiftUI
 struct ContentView: View {
     @State private var episodes = [Episode]()
     @State private var nextPage = ""
+    
     let api = API()
     
     var body: some View {
         VStack {
+            Text("Rick and Morty")
+            
             List {
                 ForEach(self.episodes, id:\.self) { episode in
-                    //NavigationLink(destination: EpisodeDetailView(episode)) {
-                    //    EpisodeRow(episode)
-                    //}
-                    
-                    Text("\(episode.name)")
-                        .onAppear {
-                            if episode == self.episodes.last && self.nextPage != "" {
-                                self.api.getEpisodes(fromURL: URL(string: self.nextPage)!) { result in
-                                    DispatchQueue.main.async {
-                                        self.episodes.append(contentsOf: result!.results)
-                                        
-                                        if result!.info.next != nil {
-                                            self.nextPage = result!.info.next!
-                                        } else {
-                                            self.nextPage = ""
-                                        }
-                                    }
-                                }
+                    NavigationLink(destination: EpisodeDetailView()) {
+                        EpisodeRow(episode)
+                            .onAppear {
+                                self.getNextEpisodes(episode)
                             }
-                        }
+                    }
                 }
             }
         }
         .onAppear {
+            // gets rid of seperators
+            UITableView.appearance().separatorStyle = .none
+            
             self.api.getEpisodes() { result in
                 DispatchQueue.main.async {
                     self.episodes = result!.results
                     self.nextPage = result!.info.next!
+                }
+            }
+        }
+    }
+    
+    func getNextEpisodes(_ episode: Episode) {
+        if episode == self.episodes.last && self.nextPage != "" {
+            self.api.getEpisodes(fromURL: URL(string: self.nextPage)!) { result in
+                DispatchQueue.main.async {
+                    self.episodes.append(contentsOf: result!.results)
+                    
+                    if result!.info.next != nil {
+                        self.nextPage = result!.info.next!
+                    } else {
+                        self.nextPage = ""
+                    }
                 }
             }
         }
--- ADT.Interview/Views/EpisodeRow.swift
+++ ADT.Interview/Views/EpisodeRow.swift
@@ -9,13 +9,36 @@
 import SwiftUI
 
 struct EpisodeRow: View {
+    var episode: Episode
+    
+    init(_ episode: Episode) {
+        self.episode = episode
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            VStack(alignment: .leading) {
+                Text("\(episode.name)")
+                    .font(Font.system(size: 16.0, weight: .bold))
+                
+                Text("\(episode.air_date)")
+                    .font(Font.system(size: 12.0))
+            }
+            .padding(.leading, 6)
+            
+            Spacer()
+            
+            Text("\(episode.episode)")
+                .font(Font.system(size: 14.0))
+        }
+        .padding(4)
+        .background(Color.gray)
+        .cornerRadius(6)
     }
 }
 
 struct EpisodeRow_Previews: PreviewProvider {
     static var previews: some View {
-        EpisodeRow()
+        EpisodeRow(Episode(id: 1, name: "name", air_date: "September 15th, 2020", episode: "S99E99", characters: [], url: "", created:""))
     }
 }
